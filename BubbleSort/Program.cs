af3647ed7ff6c98ce5aa7b67f05397f6e699485f
Sherwin Valdez <swavaldez@gmail.com>
2017-12-03 21:53:42 +0800

    Implemented Bubble Sort

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -10,6 +10,41 @@ namespace BubbleSort
     {
         static void Main(string[] args)
         {
+            int[] arr = { 3, 5, 1, 2, 7, 9, 5, 8 };
+
+            foreach (var a in arr)
+            {
+                Console.Write("{0}, ", a);
+            }
+
+            BubbleSort(arr);
+
+            Console.WriteLine(string.Empty);
+            foreach (var a in arr)
+            {
+                Console.Write("{0}, ", a);
+            }
+
+            Console.ReadKey(true);
+        }
+
+        static void BubbleSort(int[] arr)
+        {
+            int temp;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1 ] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+
+            }
+
         }
     }
 }
