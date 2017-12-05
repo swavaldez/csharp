6a6eca93501ded229c380b20ce6a8ba68774ef4f
Sherwin <swavaldez@gmail.com>
2017-12-05 11:51:20 +0800

    Implemented Queue using LinkedList

--- QueueLinkedList/Program.cs
+++ QueueLinkedList/Program.cs
@@ -10,6 +10,42 @@ namespace QueueLinkedList
     {
         static void Main(string[] args)
         {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(5);
+            queue.Enqueue(33);
+            queue.Enqueue(7);
+            queue.Enqueue(2);
+            queue.Enqueue(25);
+            queue.Enqueue(32);
+
+            foreach (var q in queue)
+            {
+                Console.WriteLine(q);
+            }
+
+            var d = queue.Dequeue();
+
+
+            Console.WriteLine("Dequeue: " + d);
+            foreach (var q in queue)
+            {
+                Console.WriteLine(q);
+            }
+
+            queue.Enqueue(31);
+
+            Console.WriteLine("Enqueue 31");
+            foreach (var q in queue)
+            {
+                Console.WriteLine(q);
+            }
+
+            var s = queue.Peek();
+            Console.WriteLine("Peek: " + s);
+
+            Console.ReadKey(true);
+
+          
         }
     }
 }
---
+++ QueueLinkedList/Queue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueLinkedList
+{
+    public class Queue<T> : IEnumerable<T>
+    {
+        private readonly LinkedList<T> _linkedList = new LinkedList<T>();
+
+
+        public void Enqueue(T data)
+        {
+            if (_linkedList.Count == 0)
+            {
+                _linkedList.AddFirst(data);
+            }
+            else
+            {
+                _linkedList.AddLast(data);
+            }
+          
+        }
+
+        public T Dequeue()
+        {
+            if(_linkedList.Count == 0) throw new InvalidOperationException("The Queue is empty.");
+
+            var value = _linkedList.First.Value;
+            _linkedList.RemoveFirst();
+
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (_linkedList.Count == 0) throw new InvalidOperationException("The Queue is empty.");
+
+            return _linkedList.First.Value;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _linkedList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
