17f063c2e19a54f5c0ae9366e19df7f3728f2b6c
Sherwin <swavaldez@gmail.com>
2017-12-06 12:03:46 +0800

    Added project for Binary Search

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,29 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
+            int[] arr = { 3, 5, 1, 2, 7, 9, 5, 8 };
+
+            foreach (var a in arr)
+            {
+                Console.Write("{0}, ", a);
+            }
+
+            InsertionSort(arr);
+
+            Console.WriteLine(string.Empty);
+            foreach (var a in arr)
+            {
+                Console.Write("{0}, ", a);
+            }
+
+            Console.ReadKey(true);
+        }
+
+        static void InsertionSort(int[] arr)
+        {
+            int selected, temp, sortkey;
+
+           
         }
     }
 }
