01ee248f5fe1e88f834bf2305dfc89544f216801
Sherwin <swavaldez@gmail.com>
2017-12-06 12:17:39 +0800

    Implemented a Binary Search

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -10,6 +10,39 @@ namespace BinarySearch
     {
         static void Main(string[] args)
         {
+            int[] arr = { 1,6,7,13,46,75,109,231,236 };
+
+            int value = BinarySearch(arr, 6);
+
+            Console.ReadKey(true);
+
+
+        }
+
+        static int BinarySearch(int[] arr, int value)
+        {
+            int low = 0, high = arr.Length - 1;
+
+            while (low < high)
+            {
+                var mid = (high + low) / 2;
+
+                if (value > arr[mid])
+                {
+                    low = mid + 1;
+                } else if (value < arr[mid])
+                {
+                    high = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+
+            return -1;
+
         }
     }
 }
